dcde0a7a5398740c58278a59dec3d66dbaecf8aa
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2019-10-01 20:25:24 -0300

    Adicionando SharedModule

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  exports: [CommonModule]
+})
+export class SharedModule {}

cc79b41fdda6312d58db4b57c312004e3de6469e
Paulo Roberto <prmorais1302@gmail.com>
2019-10-07 15:29:13 -0300

    AuthState no AuthService com RxJS ReplaySubject

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,47 +1,52 @@
-import { Injectable } from '@angular/core';
-import { Apollo } from 'apollo-angular';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Injectable } from '@angular/core'
+import { Apollo } from 'apollo-angular'
+import { Observable, ReplaySubject } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 import {
   AUTHENTICATE_USER_MUTATION,
   SIGNUP_USER_MUTATION
-} from './auth.graphql';
+} from './auth.graphql'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private apollo: Apollo) {
-    // this.signupUser({
-    //   name: 'Paulo Ricardo',
-    //   email: 'ricardo06@gmail.com',
-    //   password: 'Ricardo06'
-    // }).subscribe(res => console.log('Resposta signupUser: ', res));
+  constructor(private apollo: Apollo) {}
+
+  /*
+  Subject
+  BehaviorSubject
+  ReplaySubject
+  */
+  private _isAuthenticated = new ReplaySubject<boolean>(1)
+
+  get isAuthenticated$(): Observable<boolean> {
+    return this._isAuthenticated.asObservable()
   }
 
   signinUser(variables: {
-    email: string;
-    password: string;
+    email: string
+    password: string
   }): Observable<{ id: string; token: string }> {
     return this.apollo
       .mutate({
         mutation: AUTHENTICATE_USER_MUTATION,
         variables
       })
-      .pipe(map((res: any) => res.data.authenticateUser));
+      .pipe(map((res: any) => res.data.authenticateUser))
   }
 
   signupUser(variables: {
-    name: string;
-    email: string;
-    password: string;
+    name: string
+    email: string
+    password: string
   }): Observable<{ id: string; token: string }> {
     return this.apollo
       .mutate({
         mutation: SIGNUP_USER_MUTATION,
         variables
       })
-      .pipe(map((res: any) => res.data.signupUser));
+      .pipe(map((res: any) => res.data.signupUser))
   }
 }
